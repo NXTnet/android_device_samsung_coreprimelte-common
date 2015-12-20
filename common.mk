93e32229335d3b2af84ab0349b56e844ef3814b1
Alexandru Stefan Branetu <stefan.branetu@nxtnet.ro>
2015-12-20 03:12:26 +0200

    added permissions & init scripts

--- common.mk
+++ common.mk
@@ -135,18 +135,33 @@ PRODUCT_PACKAGES += \
 # Ramdisk
 PRODUCT_PACKAGES += \
     hcidump.sh \
+    init.ath3k.bt.sh \
     init.crda.sh \
     init.qcom.audio.sh \
     init.qcom.bt.sh \
     init.qcom.coex.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.fm.sh \
+    init.qcom.modem_links.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.uicc.sh \
     init.qcom.wifi.sh \
     init.qcom.zram.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    init.class_main.sh \
+    init.mdm.sh \
+    init.qcom.bms.sh \
+    init.qcom.class_core.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.factory.sh \
     init.qcom.rc \
+    init.qcom.sh \
+    init.qcom.syspart_fixup.sh \
     init.qcom.power.rc \
     init.qcom.usb.rc \
+    init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
