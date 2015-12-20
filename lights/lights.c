6782fff6cf847f513d58260c5ab3a31eb1beb0cd
Alexandru Stefan Branetu <stefan.branetu@nxtnet.ro>
2015-12-20 03:18:26 +0200

    revert lights.c

--- lights/lights.c
+++ lights/lights.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2011 Kolja Dummann <k.dummann@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lights.msm8916"
+#define LOG_TAG "lights.msm8960"
 
 #define LOG_NDEBUG 1
 
@@ -31,9 +31,11 @@ static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 
 char const *const LCD_FILE = "/sys/class/leds/lcd-backlight/brightness";
+char const *const BUTTON_FILE = "/sys/class/sec/sec_touchkey/brightness";
 
 void init_globals(void)
 {
+    // init the mutex
     pthread_mutex_init(&g_lock, NULL);
 }
 
@@ -85,6 +87,20 @@ static int set_light_backlight(struct light_device_t *dev, struct light_state_t 
      return err;
 }
 
+static int set_light_buttons (struct light_device_t* dev, struct light_state_t const* state) {
+     int err = 0;
+     int on = is_lit (state);
+     ALOGV("%s state->color = %d is_lit = %d", __func__,state->color , on);
+     pthread_mutex_lock (&g_lock);
+     if(on)
+         write_int(BUTTON_FILE, 1);
+     else
+         write_int(BUTTON_FILE, 0);
+
+     pthread_mutex_unlock (&g_lock);
+     return 0;
+}
+
 static int close_lights(struct light_device_t *dev)
 {
      ALOGV("close_light is called");
@@ -103,6 +119,8 @@ static int open_lights(const struct hw_module_t *module, char const *name, struc
 
      if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
+     else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
      else
         return -EINVAL;
 
@@ -132,6 +150,6 @@ struct hw_module_t HAL_MODULE_INFO_SYM = {
      hal_api_version: 0,
      id: LIGHTS_HARDWARE_MODULE_ID,
      name: "lights Module",
-     author: "CyanogenMod",
+     author: "Kolja Dummann <k.dummann@gmail.com>",
      methods: &lights_module_methods,
 };
